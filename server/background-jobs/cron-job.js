5322aa57011c22e153e556d912e225fa1bbe1b26
diditaditya <di2t.aditya@gmail.com>
2017-05-12 11:09:10 +0700

    add cron-job

--- server/background-jobs/cron-job.js
+++ server/background-jobs/cron-job.js
@@ -1,28 +1,63 @@
 const CronJob = require('cron').CronJob;
 const Kue = require('kue');
-const firebase = require('firebase');
 const queue = Kue.createQueue();
 const User = require('../models/user');
 const Match = require('../models/match');
 
-let config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-};
-
-firebase.initializeApp(config);
+function sendEmail(job, done) {
+  sendmail({
+    from: 'no-reply@futbar.com',
+    to: job.data.email,
+    subject: job.data.name,
+    html: job.data.message,
+  }, function(err, reply) {
+    console.log(err && err.stack);
+    console.dir(reply);
+  });
+  done();
+}
 
 let setCronJob = function(user, match) {
 
-  let time = match.matchTime;
+  console.log('cron-job is called!');
 
-  let cronJob = new CronJob('* * * * * *', function() {
+  let user = {};
+  if(user.local) {
+    user = user.local;
+  } else if(user.facebook) {
+    user = user.facebook;
+  }
+
+  let title = match.name;
+  let matchTime = match.matchTime;
+  let place = match.place;
+
+  let month = matchTime.getMonth();
+  let date = matchTime.getDate();
+  let hour = matchTime.getHours()-1;
+  let minute = matchTime.getMinutes();
+
+  let setTime = `0 ${minute} ${hour} ${date} ${month} *`;
+
+  let message = `${user.username}, you have futsal match ${title} at ${place} ${matchTime}! Be there and win the match!`;
+
+  let cronJob = new CronJob(setTime, function() {
+    let job = queue.create('email', {
+      message: message,
+      email: user.email,
+      subject: title
+    }).save(function(err) {
+      if(!err) console.log(job.id);
+      console.log('Error sending email');
+    });
+
+    queue.process('email', function(job, done) {
+      sendEmail(job, done);
+    });
 
   }, null, true, 'Asia/Jakarta');
 
-
 }
+
 
 module.exports = setCronJob;
