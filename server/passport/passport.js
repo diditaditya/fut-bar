44f12d7cacfb9af4f886b43674b1ba47a277d595
haryanawisnu <arya.wisnu05@gmail.com>
2017-05-12 13:46:51 +0700

    add ttd

--- server/passport/passport.js
+++ server/passport/passport.js
@@ -7,22 +7,30 @@ const jwt = require('jsonwebtoken');
 
 passport.use('local-signin', new LocalStrategy(
   function(username, password, done) {
-    User.findOne({'local.username': username}, function(err, user) {
-      if(err) {
+    User.findOne({
+      'local.username': username
+    }, function(err, user) {
+      if (err) {
         return done(err);
       }
-      if(!user) {
-        return done(null, {status: 'failed', message: 'Username is not found'});
+      if (!user) {
+        return done(null, {
+          status: 'failed',
+          message: 'Username is not found'
+        });
       }
-      if(user) {
-        console.log('given password: ', password);
-        console.log('stored password: ', user.local.password);
+      if (user) {
+        // console.log('given password: ', password);
+        // console.log('stored password: ', user.local.password);
         bcrypt.compare(password, user.local.password, function(err, res) {
-          console.log('res: ', res);
-          if(err) {
-            return done(err, {status: 'failed', message: 'Error in decoding the password-hash'});
+          // console.log('res: ', res);
+          if (err) {
+            return done(err, {
+              status: 'failed',
+              message: 'Error in decoding the password-hash'
+            });
           }
-          if(res === true) {
+          if (res === true) {
             let userInfo = {
               id: user._id,
               username: user.local.username,
@@ -31,9 +39,17 @@ passport.use('local-signin', new LocalStrategy(
             };
             let token = jwt.sign(userInfo, process.env.JWT_SECRET);
             let message = 'Sucessfully signed in';
-            return done(null, {status: 'success', message: message, user: userInfo, token: token});
+            return done(null, {
+              status: 'success',
+              message: message,
+              user: userInfo,
+              token: token
+            });
           } else {
-            return done(null, {status: 'failed', message: 'Password is incorrect'});
+            return done(null, {
+              status: 'failed',
+              message: 'Password is incorrect'
+            });
           }
         });
       }
